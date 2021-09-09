add22b038f4a9582238cee8690f45c4f11bda4af
azibodusi osain <azibodusiosain@gmail.com>
2021-09-09 11:12:14 +0200

    refactor: imported the get posts  and post method

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,10 +1,14 @@
 pub mod accounts;
 pub mod client;
 pub mod data;
+pub mod get_posts;
 pub mod image;
 pub mod me;
 pub mod pages;
+pub mod post;
 
 pub mod prelude {
-    pub use crate::graph::{accounts::*, client::*, data::*, image::*, me::*, pages::*};
+    pub use crate::graph::{
+        accounts::*, client::*, data::*, get_posts::*, image::*, me::*, pages::*, post::*,
+    };
 }
