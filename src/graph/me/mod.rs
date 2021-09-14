9e0b056ed03162029382dbf8dc4ed08b134074d6
azibodusi osain <azibodusiosain@gmail.com>
2021-09-14 11:30:49 +0200

    doc: added documentation on me module

--- src/graph/me/mod.rs
+++ src/graph/me/mod.rs
@@ -6,6 +6,10 @@ use serde::{Deserialize, Serialize};
 use crate::graph::accounts::AccountsAPI;
 use crate::graph::data::Data;
 
+/// This mod will  contain different method and struck  used to intracting with ME  API
+
+/// This struct contain different data gotten as a response  when a user sign
+
 #[derive(Deserialize, Serialize)]
 pub struct Me {
     name: String,
@@ -30,6 +34,10 @@ impl MeApi {
     pub fn accounts(self) -> AccountsAPI {
         AccountsAPI::new(self.url)
     }
+
+    /// The /me node is a special endpoint that translates to the object ID of the person or Page whose access token is currently being used
+    /// to make the API calls. If you had a User access token, you could retrieve a User's name and ID by using:
+    /// The data in the response will depend on the "Fields" parameters  you pass along the get request exmaple fields=id,name,email,picture.......
 
     pub async fn get(&self) -> seed::fetch::Result<Data<Me>> {
         log!(self.url);
