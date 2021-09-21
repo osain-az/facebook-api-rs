412aadf89ac0886a91a07a0ca52dbd5543b772e0
azibodusi osain <azibodusiosain@gmail.com>
2021-09-21 09:22:59 +0200

    feature(video posting) : added non_resumable video posting method to allow user post videos to feeds by uploading file from there systems

--- src/graph/video/mod.rs
+++ src/graph/video/mod.rs
@@ -3,43 +3,43 @@
 //! This mod contains struct and methods for uploading video to facebook API.
 //!
 //!
-use seed::fetch::fetch;
+use seed::fetch::{fetch, FormData};
 use seed::prelude::{Method, Request};
 
 use seed::{prelude::*, *};
-
+use serde_json::Value;
+use web_sys::File;
+//use gloo_file::File;
 use seed::prelude::js_sys::Math::exp;
 use serde::{Deserialize, Serialize};
 
 /// Facebook video api accepts different paramters that could be passed posted while uploading the video.
 /// this struck will have the possible pramaters that a user might need to pass along the video upload
+///
+#[derive(Deserialize, Serialize)]
 pub struct VideoParams {
-    video_title: String,
-    file_url: String, // this is when uploading with link
-    description: String,
-    sponsor_relationship: String,
-    scheduled_publish_time: String,
-    replace_video_id: String,
-    thum: String,
-    content_category: ContentCategory,
-    upload_phase: UploadPhase,
-    upload_session_id: String,
-    //uploaded_file: File,
+    ///
+    pub video_title: String,
+    pub description: String,
+    pub thum: String,
+    pub content_category: ContentCategory,
+    pub file_name: String,
+    pub title: String,
 }
 
-/// enum used for tracking the process of uploading chunked files to facebook using resumable method,
-/// each chunked upload shall contain one of this. The "start" enum is used in initializing the uploading.
-///
-enum UploadPhase {
-    start,
-    transfer,
-    finish,
-    cancel,
+pub struct UploadFile {
+    pub file: File,
 }
 
 /// Enum for the different categories that the uploaded video will belong to as difined on facebook  graph api documentation.
 /// Choose any from the list, if  no data is supplied a default value of  "OTHER" is choosen.
 ///
+#[derive(Deserialize, Serialize)]
+pub struct PostResponse {
+    id: String,
+}
+
+#[derive(Deserialize, Serialize)]
 pub enum ContentCategory {
     EAUTY_FASHION,
     BUSINESS,
@@ -60,40 +60,141 @@ pub enum ContentCategory {
     VIDEO_GAMING,
     OTHER,
 }
+
 impl Default for VideoParams {
     /// this default method will create the struct will default
     fn default() -> Self {
         Self {
-            video_title: "".to_string(),
-            file_url: "".to_string(),
-            description: "".to_string(),
-            sponsor_relationship: "".to_string(),
-            scheduled_publish_time: "".to_string(),
-            replace_video_id: "".to_string(),
+            video_title: "this video we post".to_string(),
+            description: "This video is for testing purpose".to_string(),
             thum: "".to_string(),
             content_category: ContentCategory::OTHER,
-            upload_phase: UploadPhase::start,
-            upload_session_id: "".to_string(),
+            file_name: "file".to_string(),
+            title: " tThis is the best video all said ".to_string(),
         }
     }
 }
 
 impl VideoParams {
-    fn new() -> VideoParams {
+    pub fn new() -> VideoParams {
         VideoParams::default()
     }
+    pub fn update_video_params(mut self, video_params: VideoParams) -> Self {
+        Self { ..video_params }
+    }
 
-    /*   pub fn init_video_upload(self, base_url: String) {
-        let file_size_byte = self.uploaded_file.metadata().unwrap().len();
-        let  file_size_gb =  file_size_byte/
-        let  non_resumable_max_size_gb =  0.9; //Gb: facebook recommmended max 1 Gb for none resumabl upload
-        if self.uploaded_file.metadata().unwrap().len()+ > exp(6) {}
-    }*/
-    /*
-    pub async fn non_resumable_upload(&self, file: File) -> seed::fetch::Result<FeedPostSuccess> {
+    pub fn get_video_params(self) -> VideoParams {
+        self
+    }
+
+    fn create_form_data(self, upload_phase: String, chunked_file: File) -> FormData {
+        // better way should be a loop  and append the data to the form
+        let mut form_data = FormData::new(); // init empty form data
+        form_data.append_blob("source", &chunked_file); // appped  the  current chunked file   to the form
+
+        if !self.video_title.is_empty() {
+            form_data.append_str("video_title", &self.video_title);
+        }
+
+        if !self.description.is_empty() {
+            form_data.append_str("description", &self.description);
+        }
+
+        if !self.thum.is_empty() {
+            form_data.append_str("thum", &self.thum);
+        };
+        if !self.title.is_empty() {
+            form_data.append_str("title", &self.title);
+        };
+        form_data
+    }
+}
+
+#[derive(Deserialize, Default, Serialize)]
+
+pub struct VideoApi {
+    base_url: String,
+    page_access_token: String,
+    video_post_type: String,
+    video_params: Option<VideoParams>,
+}
+
+impl VideoApi {
+    pub fn new(base_url: String, page_access_token: String) -> VideoApi {
+        VideoApi {
+            base_url,
+            page_access_token,
+            video_post_type: "".to_string(),
+            video_params: None,
+        }
+    }
+
+    /// This method accepts video file in a struct  and VideoPrams struct  which will then decide either using resumable video upload or non_resumable method
+    /// as defined by facebook  video upload api.
+
+    pub fn init_video_upload(
+        mut self,
+        upload_file: UploadFile,
+        video_params: VideoParams,
+    ) -> VideoApi {
+        let non_resumable_max_size_gb = 0.9; //Gb: facebook recommmended max 1 Gb for none resumabl upload
+        let resumable_max_size_gb = 4.0; //Gb: facebook recommmended max max of 4  Gb for  resumabl uploading video
+        let file = upload_file.file;
+        //self.video_file   = file;
+        let file_size_byte = file.size() as f64; // file size in byte
+        let file_size_gb = file_size_byte / 10_f64.powf(9.0); // convert the file to Gb
+        if file_size_gb < non_resumable_max_size_gb {
+            self.video_post_type = "non_resumable".to_string();
+            self.video_params = Some(video_params);
+            self
+        } else {
+            // this will be for larger videos
+            self.video_post_type = "non_resumable".to_string();
+            self.video_params = None;
+            self
+        }
+    }
+
+    fn create_form_data(
+        upload_phase: String,
+        chunked_file: File,
+        video_params: VideoParams,
+    ) -> FormData {
+        let mut form_data = FormData::new();
+        form_data.append_blob("source", &chunked_file); // appped  the  current chunked file   to the form
+
+        if !video_params.video_title.is_empty() {
+            form_data.append_str("video_title", &video_params.video_title);
+        }
+
+        if !video_params.description.is_empty() {
+            form_data.append_str("description", &video_params.description);
+        }
+
+        if !video_params.thum.is_empty() {
+            form_data.append_str("thum", &video_params.thum);
+        };
+
+        form_data
+    }
+    ///This method
+    pub async fn post_video(self, uploaded_file: UploadFile) -> seed::fetch::Result<PostResponse> {
+        if self.video_post_type == "non_resumable" {
+            VideoApi::non_resumable_video_upload(&self, uploaded_file).await
+        } else {
+            // this will be when uploading large file
+            VideoApi::non_resumable_video_upload(&self, uploaded_file).await
+        }
+    }
+    pub async fn non_resumable_video_upload(
+        &self,
+        uploaded_file: UploadFile,
+    ) -> seed::fetch::Result<PostResponse> {
+        let form_data =
+            VideoApi::create_form_data("".to_string(), uploaded_file.file, Default::default());
         let base_url = self.base_url.replace("EDGE", "videos");
-        let url = base_url + "?file_url=" + file_url + "&access_token=" + &self.access_token;
-        let request = Request::new(url).method(Method::Post);
-        fetch(request).await?.json::<FeedPostSuccess>().await
-    }*/
+        let url = base_url + "?access_token=" + &self.page_access_token;
+        let request = Request::new(url).method(Method::Post).form_data(form_data);
+        fetch(request).await?.json::<PostResponse>().await
+    }
 }
