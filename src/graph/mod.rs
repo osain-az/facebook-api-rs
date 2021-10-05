1aca5baff6e549bb186b8983084ed710a6f25a9d
azibodusi osain <azibodusiosain@gmail.com>
2021-10-05 18:16:05 +0200

    refactor: import and export the utils and video mod

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -3,15 +3,15 @@ pub mod client;
 pub mod data;
 pub mod get_posts;
 pub mod image;
+pub mod instagram;
 pub mod me;
 pub mod pages;
 pub mod post;
-pub mod video;
 pub mod utils;
+pub mod video;
 pub mod prelude {
     pub use crate::graph::{
-        accounts::*, client::*, data::*,
-        get_posts::*, image::*, me::*, pages::*,
-        post::*, video::*, utils::*,
+        accounts::*, client::*, data::*, get_posts::*, image::*, instagram::*, me::*, pages::*,
+        post::*, utils::*, video::*,
     };
 }
