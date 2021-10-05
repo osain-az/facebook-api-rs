8871628a6459471c15c5af47e044050a4c5ec264
azibodusi osain <azibodusiosain@gmail.com>
2021-10-05 18:29:24 +0200

    refactor: added post filed struct

--- src/graph/post/mod.rs
+++ src/graph/post/mod.rs
@@ -29,16 +29,58 @@ pub struct MediaPostSuccess {
 
 ///This struct i sthe
 
-/*pub struct VideoParams {
-    video_title: String,
-    file_url: String,
-    description: String,
-    sponsor_relationship: String,
-    scheduled_publish_time: String,
-    replace_video_id: String,
-    thum: String,
-    content_category: ContentCategory,
-}*/
+pub struct PostFields {
+    link: String,
+    call_to_action: CallToAction,
+    action: CallToActionList,
+}
+pub enum CallToActionList {
+    //Determines the call to action button text. One of allowed values:
+    BOOK_TRAVEL,
+    ///. Call to action shows up as Book Now.
+    BUY_NOW,
+    /// Call to action shows up as Buy Now. Only used for desktop app ads for virtual goods.
+    CALL_NOW,
+    ///Call to action shows up as Call Now. Only used for local awareness ads.
+    DOWNLOAD,
+    ///  Call to action shows up as Download.
+    GET_DIRECTIONS,
+    /// Call to action shows up as Get Directions. Must specify coordinates on the link field. Only used for local awareness ads.
+    GET_QUOTE,
+    /// Call to action shows up as Get Quote for lead generation.
+    INSTALL_APP,
+    /// Call to action shows up as Install Now.
+    INSTALL_MOBILE_APP, // Call to action shows up as Install Now. Only used for mobile app ads.
+
+    LEARN_MORE, // Call to action shows up as Learn More.
+
+    LIKE_PAGE, // Call to action shows up as Like Page. Only used for ads in Page Likes objective.
+
+    LISTEN_MUSIC, // Call to action shows up as Listen Music.
+
+    MESSAGE_PAGE, // Call to action shows up as Send Message. Only used for local awareness ads.
+
+    NO_BUTTON, //No call to action shows up.
+
+    OPEN_LINK, //Call to action shows up as Open Link. Only used for ads in Website Clicks objective.
+
+    PLAY_GAME, // Call to action shows up as Play Game. Only used for desktop app ads.
+
+    SHOP_NOW,
+    ///all to action shows up as Shop Now. Only used for ads in Website Conversions objective.
+    SIGN_UP,
+    ///Cl to action shows up as Sign Up.
+    SUBSCRIBE, // Call to action shows up as Subscribe for lead generation.
+
+    USE_APP,
+    ///Cl to action shows up as Use App.
+    USE_MOBIL, //_APP. Only used for mobile app ads.
+
+    WATCH_MOR, //. Call to action shows up as Watch More.
+
+    WATCH_VID, //O. Call to action shows up as Watch Video.
+}
+pub struct CallToAction {}
 
 /// Return response for posting feeds ( picture or video) to a page or user account feed
 ///
