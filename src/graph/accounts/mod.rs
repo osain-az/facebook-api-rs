773ac8a535df70c6bacb57a0804ca566f2602aaa
azibodusi osain <azibodusiosain@gmail.com>
2021-10-12 09:26:43 +0200

    doc: added more detailed documentation to the accounts

--- src/graph/accounts/mod.rs
+++ src/graph/accounts/mod.rs
@@ -2,20 +2,29 @@ use crate::graph::data::Data;
 use seed::{prelude::*, *};
 use serde::{Deserialize, Serialize};
 
-///This struct represent the that data gotten from the response when  the account API is called
-/// it returns the  Facebook Pages that a person owns or is able to perform tasks on.
-/// the response is the list of pages and the atskes the user can perform on those pages
+/// This struct represent the that data gotten from the response when  the
+/// account API is called, it returns the  Facebook Pages that a user owns or
+/// is able to perform tasks on. the response is the list of pages and the
+/// taskes, information and action the user can perform on those pages as shown
+/// in the struct. the actual response is varies with pages but this struct data
+/// will be present in every page For more details check facebook official documentation     https://developers.facebook.com/docs/graph-api/reference/user/accounts/
 
 #[derive(Deserialize, Debug, Default, Serialize)]
 pub struct Accounts {
+    /// The access token of this given page, which used to make operation that
+    /// requires permission on this page example post and get request.
     pub access_token: String,
-    category: String,
-    category_list: Vec<ListDetails>,
+    pub category: String,
+    pub category_list: Vec<ListDetails>,
+    /// page name
     pub name: String,
     pub id: String,
-    tasks: Vec<String>,
+    /// this is the list of operation/task the user can perform on this page
+    pub tasks: Vec<String>,
 }
-//Todo add documentation on every field.
+
+/// This is the struct of name and id of category that page belong to
+
 #[derive(Deserialize, Debug, Default, Serialize)]
 pub struct ListDetails {
     id: String,
@@ -23,25 +32,33 @@ pub struct ListDetails {
 }
 
 impl Accounts {
-    pub fn get_access_token(&self) -> &String {
+    /// This will return the page access token
+    pub fn access_token(&self) -> &String {
         &self.access_token
     }
-    pub fn get_category(&self) -> &String {
+
+    pub fn category(&self) -> &String {
         &self.category
     }
-    pub fn get_category_list(&self) -> &Vec<ListDetails> {
+
+    /// This will return the list of category the page  belong
+    pub fn category_list(&self) -> &Vec<ListDetails> {
         &self.category_list
     }
 
-    pub fn get_name(&self) -> &String {
+    /// This will return the page name
+    pub fn name(&self) -> &String {
         &self.name
     }
 
-    pub fn get_id(&self) -> &String {
+    /// This will return the page id
+    pub fn id(&self) -> &String {
         &self.id
     }
 
-    pub fn get_tasks(&self) -> &Vec<String> {
+    /// This will returned the list of permission and tasks the user is
+    /// permitted to perform on the page
+    pub fn tasks(&self) -> &Vec<String> {
         &self.tasks
     }
 }
@@ -60,6 +77,17 @@ impl AccountsAPI {
     pub fn url(&self) -> &str {
         &self.url
     }
+
+    /// This request  will get the list of  Facebook Pages that a person owns or
+    /// is able to perform tasks on.
+    ///
+    /// The response data can be seen in the Accounts struct,   the struct is
+    /// constructed to only take data that will be returned on every page
+    /// since the actual reponse from facebook varies with pages
+    ///
+    /// To check the possoble data that is possible to be in the response which
+    /// varies with pages check facebook documentation     https://developers.facebook.com/docs/graph-api/reference/user/accounts/
+
     pub async fn get(&self) -> seed::fetch::Result<Data<Accounts>> {
         log!(self.url);
         let request = Request::new(&self.url).method(Method::Get);
