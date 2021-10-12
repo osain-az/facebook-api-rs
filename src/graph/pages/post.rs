db45ba9288143d985e6f84d31092f9024c6de414
azibodusi osain <azibodusiosain@gmail.com>
2021-10-12 16:35:55 +0200

    feature(update, delete): started working on the implementation for updating and deleting of a given post

---
+++ src/graph/pages/post.rs
@@ -0,0 +1,10 @@
+//! parent page reference       https://developers.facebook.com/docs/graph-api/reference/v12.0/page/feed#read
+// child page   reference       https://developers.facebook.com/docs/graph-api/reference/pagepost
+
+// this is used for performing  different task on page  limited to the following
+
+// 1   geting post by page_post_id
+// 2 : updating a single post
+// 2 : deleting a single post
+
+// note Noted creating is allowed on these  end point
