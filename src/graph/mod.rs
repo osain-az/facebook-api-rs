06c3811de2d82e4cdecaab45837284f11c0d86b1
azibodusi osain <azibodusiosain@gmail.com>
2021-10-12 21:03:55 +0200

    refactor: removed the "get_post" import

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,17 +1,13 @@
 pub mod accounts;
 pub mod client;
 pub mod data;
-pub mod get_posts;
 pub mod image;
 pub mod instagram;
 pub mod me;
 pub mod pages;
-pub mod post;
 pub mod utils;
-pub mod video;
 pub mod prelude {
     pub use crate::graph::{
-        accounts::*, client::*, data::*, get_posts::*, image::*, instagram::*, me::*, pages::*,
-        post::*, utils::*, video::*,
+        accounts::*, client::*, data::*, image::*, instagram::*, me::*, pages::*, utils::*,
     };
 }
