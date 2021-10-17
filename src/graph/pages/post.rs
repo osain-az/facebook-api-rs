83f47923c508c65cb4f9e28b9cfd07990d4ad79f
azibodusi osain <azibodusiosain@gmail.com>
2021-10-17 19:46:26 +0200

    refactor: moved the get post method to post.rs and updated the method

--- src/graph/pages/post.rs
+++ src/graph/pages/post.rs
@@ -1,10 +1,52 @@
-//! parent page reference       https://developers.facebook.com/docs/graph-api/reference/v12.0/page/feed#read
-// child page   reference       https://developers.facebook.com/docs/graph-api/reference/pagepost
+/// The Post API let  you perform operation on individual post in a profile's
+/// feed. The profile could be a user, page, app, or group. (get post, update);
+/// Note: This end point does not allow you to create post or delete.
+/// for more details check facebook documentation on post Api.
+/// https://developers.facebook.com/docs/graph-api/reference/post
+use crate::graph::pages::utils::{Fields, GetPostResponse};
+use seed::fetch::fetch;
+use seed::prelude::{Method, Request};
+use seed::{prelude::*, *};
+use serde::{Deserialize, Serialize};
 
-// this is used for performing  different task on page  limited to the following
+use seed::{prelude::*, *};
 
-// 1   geting post by page_post_id
-// 2 : updating a single post
-// 2 : deleting a single post
+/// The Post API let  you perform operation on individual post in a profile's
+/// feed. The profile could be a user, page, app, or group. (get post, update);
+/// Note: This end point does not allow you to create post or delete.
+/// for more details check facebook documentation on post Api.
+/// https://developers.facebook.com/docs/graph-api/reference/post
+pub struct PostApi {
+    base_url: String,
+    page_access_token: String,
+}
 
-// note Noted creating is allowed on these  end point
+impl PostApi {
+    /// this is a static method used to create an instance of the feedApi
+    /// Note: this method is called inside of the Client method
+    pub fn new(base_url: String, access_token: String) -> PostApi {
+        PostApi {
+            base_url,
+            page_access_token: access_token,
+        }
+    }
+
+    ///  this method sends a get request to the facebook api (GET
+    /// /v12.0/{post-id}). it returns the data of the post_id  you have
+    /// provided
+    pub async fn get(self) -> seed::fetch::Result<GetPostResponse> {
+        let mut url = self.base_url.replace("EDGE", "?fields=");
+
+        let field_count = Fields::default().fields.len();
+        for (count, field) in Fields::default().fields.into_iter().enumerate() {
+            if count < field_count - 1 {
+                url = url + &field + ",";
+            } else {
+                url = url + &field; // remove the comma in the last filed
+            }
+        }
+        let base_url = url + "&access_token=" + &self.page_access_token;
+        let request = Request::new(base_url).method(Method::Get);
+        fetch(request).await?.json::<GetPostResponse>().await
+    }
+}
