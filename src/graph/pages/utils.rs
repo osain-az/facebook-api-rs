54683c27c21674f54e5f65030d8611d9209345c9
azibodusi osain <azibodusiosain@gmail.com>
2021-10-17 19:53:07 +0200

    refactor: moved the fields parameters used by feed and post file to utils in the page folder

---
+++ src/graph/pages/utils.rs
@@ -0,0 +1,45 @@
+use seed::fetch::fetch;
+use seed::prelude::{Method, Request};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug, Serialize)]
+pub struct Fields {
+    pub(crate) fields: Vec<String>,
+}
+
+impl Default for Fields {
+    /// This parameters are used as fields which are passed in as a query
+    /// parameters to the get post request and feeds request  
+    fn default() -> Self {
+        let field_list = vec![
+            "from",
+            "id",
+            "message_tags",
+            "story",
+            "story_tags",
+            "permalink_url",
+            "message",
+            "shares",
+            "comments",
+            "likes",
+            "reactions",
+        ];
+        let fields = field_list.iter().map(|&field| field.into()).collect();
+        Self { fields }
+    }
+}
+
+/// expected fields gotten from the get post request
+#[derive(Deserialize, Debug, Default, Serialize)]
+pub struct GetPostResponse {
+    pub id: String,
+    pub message: String,
+    pub from: From,
+    pub permalink_url: String,
+}
+
+#[derive(Deserialize, Debug, Default, Serialize)]
+pub struct From {
+    pub id: String,
+    pub name: String,
+}
