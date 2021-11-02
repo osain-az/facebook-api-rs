254007d97b08c5ddfb2299b2399732db6ded8826
azibodusi osain <azibodusiosain@gmail.com>
2021-11-02 11:03:53 +0100

    feature(instagram_account): added a method used to getting details information of instagram account

--- src/graph/instagram/account/mod.rs
+++ src/graph/instagram/account/mod.rs
@@ -3,6 +3,8 @@ use seed::prelude::{Method, Request};
 
 use seed::{prelude::*, *};
 use serde::{Deserialize, Serialize};
+use seed::prelude::js_sys::encode_uri;
+use urlencoding::encode;
 
 #[derive(Deserialize, Clone, Debug, Serialize)]
 pub struct InstaAccount {
@@ -16,10 +18,12 @@ pub struct InstaAccountId {
     pub id: String,
 }
 
+#[derive( Clone)]
 pub struct InstagramApi {
     page_access_token: String,
     base_url: String,
 }
+
 
 impl InstagramApi {
     pub fn new(page_access_token: String, base_url: String) -> InstagramApi {
@@ -29,7 +33,7 @@ impl InstagramApi {
         }
     }
 
-    pub async fn insta_account(self) -> seed::fetch::Result<InstaAccount> {
+    pub async fn account_id(self) -> seed::fetch::Result<InstaAccount> {
         let base_url = self.base_url.replace("EDGE", "?");
         let url = base_url
             + "fields=instagram_business_account"
@@ -39,4 +43,113 @@ impl InstagramApi {
         let request = Request::new(url).method(Method::Get);
         fetch(request).await?.json::<InstaAccount>().await
     }
+
+    pub async fn account_details(self) -> seed::fetch::Result<InstagramAccount> {
+        let base_url = "https://graph.facebook.com/v11.0/";
+
+         let self_data = self.clone();
+
+       let result =  self.account_id().await;
+        if result.is_ok(){
+            let instagram_id = result.unwrap();
+            let mut url = base_url.to_owned()
+                + &instagram_id.instagram_business_account.id.as_str();
+
+            // added required filed needed
+            let mut fields_count = Fields::default().fields.len();
+             let mut url_fields = "".to_string();
+            for (count, field) in Fields::default().fields.into_iter().enumerate() {
+                if count < fields_count - 1 {
+                    url_fields = url_fields+ &field + ",";
+                } else {
+                    url_fields= url_fields+ &field; // remove the comma in the last filed
+                }
+            }
+            url_fields = String::from(encode(url_fields.as_str()));
+
+             let mut request_url = url
+                 + "?fields="
+                 +url_fields.as_str()
+                + "&access_token="
+                + &self_data.page_access_token;
+
+               log!(request_url);
+            let request = Request::new(request_url).method(Method::Get);
+           fetch(request).await?.json::<InstagramAccount>().await
+
+        }else {
+            let err = JsValue::from_str("The first get request call to fetch the instagram id was not successful,\
+             therefore the request to get instagram account failed. Try making the request again.");
+            Err(FetchError::RequestError(err)) // try to generate a customer
+            // error
+        }
+
+    }
+
+
 }
+
+
+#[derive(Deserialize, Clone, Debug, Serialize)]
+pub struct InstagramAccount {
+    //https://developers.facebook.com/docs/instagram-api/reference/ig-user/
+    //This is a public fields, which means it can be returned if available.
+  //  biography: String,
+
+    ///This is a public fields, which means it can be returned if available.
+  pub  id:  String,
+
+    ///This is not a public fields, which means it may be returned depending on the user setting.
+    pub  ig_id: u32,
+
+    ///This is a public fields, which means it can be returned if available.
+    pub  followers_count:  u32,
+
+    ///This is not a public fields, which means it may be returned depending on the user setting.
+    pub  follows_count : u32,
+
+    ///This is not a public fields, which means it may be returned depending on the user setting.
+    pub   media_count: u32,
+
+    //This is not a public fields, which means it may be returned depending on the user setting.
+    pub   name: String,
+
+    //This is not a public fields, which means it may be returned depending on the user setting.
+  //  profile_picture_url:  String,
+
+    ///This is not a public fields, which means it may be returned depending on the user setting.
+    pub  username : String,
+
+    //This is not a public fields, which means it may be returned depending on the user setting.
+   // website :  String,
+}
+pub struct Fields {
+    pub(crate) fields: Vec<String>,
+}
+
+impl Default for Fields {
+    /// This parameters are used as fields which are passed in as a query
+    /// parameters to the get post request and feeds request
+    fn default() -> Self {
+        let field_list = vec![
+            "biography",
+            "id",
+            "ig_id",
+            "followers_count",
+            "follows_count",
+            "media_count",
+            "name",
+            "profile_picture_url",
+            "username",
+            "website",
+        ];
+
+        let fields = field_list.iter().map(|&field| field.into()).collect();
+        Self { fields }
+    }
+}
+// This is only used for serialize
+/*#[allow(clippy::trivially_copy_pass_by_ref)]
+fn is_undefined(num: String) -> bool {
+    *num ==
+}*/
